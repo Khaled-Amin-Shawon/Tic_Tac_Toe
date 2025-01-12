ea3a5f62b8a9040132c22a1ab9a867cfb522c17d
Khaled-Amin-Shawon <mdkhaledaminshawon@gmail.com>
2025-01-13 05:54:36 +0600

    Implement game logic and UI for Tic Tac Toe, including win/draw checks and reset functionality

--- lib/Pages/game_screen.dart
+++ lib/Pages/game_screen.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 
 class GameScreen extends StatefulWidget {
@@ -10,6 +11,95 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+  late List<List<String>> _board;
+  late String _currentPlayer;
+  late String _winner;
+  late bool _gameOver;
+
+  @override
+  void initState() {
+    super.initState();
+    _board = List.filled(3, List.filled(3, ''));
+    _currentPlayer = widget.player1;
+    _winner = '';
+    _gameOver = false;
+  }
+
+  // Reset the game state
+  void _resetGame() {
+    _board = List.filled(3, List.filled(3, ''));
+    _currentPlayer = widget.player1;
+    _winner = '';
+    _gameOver = false;
+  }
+
+  // Check if a player has won the game
+  void _makeMove(int row, int col) {
+    if (_board[row][col] != '' || _gameOver) return;
+    _board[row][col] = _currentPlayer;
+    setState(() {
+      if (_checkWin(row, col)) {
+        _winner = _currentPlayer;
+        _gameOver = true;
+      } else if (_checkDraw()) {
+        _winner = 'It\'s a draw!';
+        _gameOver = true;
+      } else {
+        _currentPlayer =
+            _currentPlayer == widget.player1 ? widget.player2 : widget.player1;
+      }
+      if (_gameOver) {
+        // Add your winning/drawing animation here
+        AwesomeDialog(
+          context: context,
+          dialogType: DialogType.success,
+          animType: AnimType.rightSlide,
+          btnOkText: "Play Again...",
+          title: _winner.isNotEmpty
+              ? '${_winner == widget.player1 ? widget.player1 : widget.player2} Won!'
+              : "It's a draw!",
+          btnOkOnPress: _resetGame,
+        )..show();
+      }
+    });
+  }
+
+  bool _checkWin(int row, int col) {
+    // Add your win-checking logic here
+    if (
+        // Check rows
+        _board[row][col] == _currentPlayer &&
+                _board[row][0] == _board[row][1] &&
+                _board[row][1] == _board[row][2] ||
+            // Check columns
+            _board[col][row] == _currentPlayer &&
+                _board[0][row] == _board[1][row] &&
+                _board[1][row] == _board[2][row] ||
+            // Check diagonals 1
+            _board[0][0] == _currentPlayer &&
+                _board[1][1] == _currentPlayer &&
+                _board[2][2] == _currentPlayer ||
+            // Check diagonals 2
+            _board[0][2] == _currentPlayer &&
+                _board[1][1] == _currentPlayer &&
+                _board[2][0] == _currentPlayer) {
+      return true;
+    }
+    return false;
+  }
+
+  bool _checkDraw() {
+    // Add your draw-checking logic here
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < 3; j++) {
+        if (_board[i][j] == '') {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
